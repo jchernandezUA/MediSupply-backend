9268e391cf3aaaaa3e0029b104ff24240299b767
nicolaslz <nico.ing.mec.2@gmail.com>
2025-10-17 11:04:33 -0500

    Limpieza de pruebas, servicios y routes

--- vendedores_microservice/app/__init__.py
+++ vendedores_microservice/app/__init__.py
@@ -4,8 +4,6 @@ from .models import db
 from .routes.errors import register_error_handlers
 from .routes.health import bp_health
 from .routes.vendedores import bp_vendedores
-from .routes.planes import bp_planes
-from .routes.asignaciones import bp_asignaciones
 
 def create_app():
     app = Flask(__name__)
@@ -25,8 +23,6 @@ def create_app():
     # Blueprints
     app.register_blueprint(bp_health, url_prefix="/v1")
     app.register_blueprint(bp_vendedores, url_prefix="/v1")
-    app.register_blueprint(bp_planes, url_prefix="/v1")
-    app.register_blueprint(bp_asignaciones, url_prefix="/v1")
 
     # Errores → HTTP
     register_error_handlers(app)
